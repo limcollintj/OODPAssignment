47079183c68c096c1b7fb54187e7ed9a76c67efe
Jun Meng <tanjunmeng@gmail.com>
2018-11-06 00:08:14 +0800

    Updated some javadocs on functionality classes

--- src/functionalityClasses/CRUDByID.java
+++ src/functionalityClasses/CRUDByID.java
@@ -1,10 +1,43 @@
 package functionalityClasses;
 
-import courses.Course;
+
+/**
+ Interface for creating, reading, updating and deleting by ID.
+ @author LFM
+ @version 1.0
+ @since 2018-11-05
+ */
+
 
 public interface CRUDByID {
+	/**
+	 * Create ____ by ID.
+	 * @param id
+	 * @throws Exception
+	 */
 	void createByID(String id) throws Exception;
+
+	/**
+	 * Read ____ by ID.
+	 * @param id
+	 * @return
+	 * @throws Exception
+	 */
 	Object readByID(String id) throws Exception;
+
+	/**
+	 * Update ____ by ID.
+	 * @param id
+	 * @throws Exception
+	 */
 	void updateByID(String id) throws Exception;
+
+	/**
+	 * Delete ____ by ID.
+	 * @param id
+	 * @throws Exception
+	 */
 	void deleteByID(String id) throws Exception;
+
 }
+
--- src/functionalityClasses/PrintCourseByID.java
+++ src/functionalityClasses/PrintCourseByID.java
@@ -5,9 +5,21 @@ import students.Student;
 import students.StudentManager;
 import util.DatabaseHandler;
 
+
+/**
+ Class for printing list of courses by ID.
+ @author LFM
+ @version 1.0
+ @since 2018-11-05
+ */
+
 public class PrintCourseByID implements PrintByID{
 
-
+	/**
+	 * Print courses by ID.
+	 * @param id
+	 * @throws Exception
+	 */
 	@Override
 	public void printByID(String id) throws Exception {
 		
