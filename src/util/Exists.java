e1be969a24533cfc4da68e99d7b2af64ff8dd4c8
Jordan <jliew008@e.ntu.edu.sg>
2018-11-16 04:02:37 +0800

    Delete all container files

--- src/util/Exists.java
+++
@@ -1,5 +0,0 @@
-package util;
-
-public interface Exists {
-	public boolean exists(String id);
-}
--- src/util/Findable.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-import java.util.List;
-
-public interface Findable {
-	public Object findObjectByID(String id);
-	public List<Object> findAllByID(String id);
-}
